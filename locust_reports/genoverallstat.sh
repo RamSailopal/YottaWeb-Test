ec60fb3dff4244e958668343064cb3cbf15ded87
berwyn <berwyn.hoyt@gmail.com>
2022-08-30 15:26:38 +1200

    Overall report now links the detail reports so that README doesn't have to list every report specifically

--- locust_reports/genoverallstat.sh
+++ locust_reports/genoverallstat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp -f statsmaster.html overallstats.html
-awk --file awkscript $(find -name '*.html' ! -name statsmaster.html ! -name overallstats.html)  >> overallstats.html
+awk --file awkscript $(find -name '*.html' ! -name statsmaster.html ! -name overallstats.html | sort)  >> overallstats.html
